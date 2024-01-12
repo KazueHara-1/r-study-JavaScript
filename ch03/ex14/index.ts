37c856aaef44a99e090bfc4515f8cb38ad36b943
Hara Kazue (原 和江) <Kazue.Hara@jp.ricoh.com>
2024-01-12 14:30:03 +0900

    休憩時間まで

--- ch03/ex14/index.ts
+++ ch03/ex14/index.ts
@@ -49,27 +49,22 @@ export function lte(a: unknown, b: unknown) {
   //   。オペランドの一方がオブジェクトの場合、数値優先アルゴリズムを使ってオブジェ
   // クトを基本型値に変換します。ただし、オブジェクトから数値への変換とは異なり、数値優先アル
   // ゴリズムで返された基本型値は、数値には変換されません。
-  if (typeof a === 'number' && typeof b === 'number') {
+  if (
+    (typeof a === 'number' && typeof b === 'number') ||
+    (typeof a === 'string' && typeof b === 'string')
+  ) {
     if (a < b || a === b) {
       return true;
     }
     return false;
   }
-  let modifiedA = a;
-  let modifiedB = b;
+  // let modifiedA = a;
+  // let modifiedB = b;
 
-  if (typeof a === 'string') {
-    modifiedA = Number(a);
-  }
-  if (typeof b === 'string') {
-    modifiedB = Number(b);
-  }
-  console.log(modifiedA);
-  console.log(modifiedB);
-  if (typeof modifiedA === 'number' && typeof modifiedB === 'number') {
-    if (modifiedA < modifiedB || modifiedA === modifiedB) {
-      return true;
-    }
-  }
+  // if (typeof modifiedA === 'number' && typeof modifiedB === 'number') {
+  //   if (modifiedA < modifiedB || modifiedA === modifiedB) {
+  //     return true;
+  //   }
+  // }
   return false;
 }
