4317dbfed74cc735538736841d7f5fa00aecef47
Kazue Hara <Kazue.Hara@jp.ricoh.com>
2024-06-19 12:23:49 +0900

    ex14途中まで

---
+++ ch13/ex14/index.ts
@@ -0,0 +1,64 @@
+export class PromisePool {
+  status: "start" | "stop" = "stop";
+  queSize: number;
+  maxRunningPromises: number;
+  runningPromises: (() => Promise<void>)[] = [];
+  que: (() => Promise<void>)[] = [];
+  /**
+   * Constructs PromisePool.
+   *
+   * @param queueSize the max size of queue
+   * @param maxRunningPromises the maximum number of running promises at the same time.
+   *
+   * @throws Error if either queueSize or maxRunningPromises is less than 1
+   */
+  constructor(queueSize: number, maxRunningPromises: number) {
+    if (queueSize < 1) {
+      throw new Error("queueSize must be larger than 0");
+    } else if (maxRunningPromises < 1) {
+      throw new Error("maxRunningPromises must be larger than 0");
+    }
+    this.queSize = queueSize;
+    this.maxRunningPromises = maxRunningPromises;
+  }
+
+  /**
+   * Starts PromisePool.
+   *
+   * @returns Promise, which will be rejected if this pool is already started
+   */
+  async start() {
+    if (this.status === "start") {
+      throw new Error("PromisePool is already started.");
+    }
+    this.status = "start";
+  }
+
+  /**
+   * Wait all promises for their terminations.
+   * All requests dispatched before this method is invoked must complete
+   * and this method also will wait for their completion.
+   *
+   * @returns Promise, which will be rejected if this pool has not been started.
+   */
+  async stop() {
+    if (this.status === "stop") {
+      throw new Error("PromisePool is not started.");
+    }
+    this.status = "stop";
+  }
+
+  /**
+   * Executes the specified promise from the given factory using this pool.
+   * If the queue is full, then the returned Promise will not be fulfilled until the queue is not full.
+   *
+   * @param promiseFactory the function that returns Promise
+   *
+   * @returns Promise, which will be rejected if this pool has not been started.
+   */
+  async dispatch(promiseFactory: () => Promise<void>): Promise<void> {
+    if (this.status === "stop") {
+      throw new Error("PromisePool is not started.");
+    }
+  }
+}
