75d9958f162ee47bbc1e6d38f91e1e84d98344ac
Kazue Hara <Kazue.Hara@jp.ricoh.com>
2024-08-10 17:01:34 +0900

    ex06

--- ch15.04-10/ex06/index.js
+++ ch15.04-10/ex06/index.js
@@ -21,6 +21,35 @@ class TodoApp extends HTMLElement {
 
     this.form = this.shadowRoot.querySelector("#new-todo-form");
     // TODO: 残りを実装
+    this.input = this.shadowRoot.querySelector("input");
+    const list = this.shadowRoot.querySelector("#todo-list");
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      if (this.input.value.trim() === "") {
+        return;
+      }
+      const todo = this.input.value.trim();
+      this.input.value = "";
+
+      const li = document.createElement("li");
+      const toggle = document.createElement("input");
+      toggle.type = "checkbox";
+      const label = document.createElement("label");
+      li.appendChild(label);
+      const destroy = document.createElement("button");
+      li.appendChild(destroy);
+      destroy.textContent = "✖";
+
+      toggle.addEventListener("change", () => {
+        li.classList.toggle("completed", toggle.checked);
+      });
+      label.textContent = todo;
+      destroy.addEventListener("click", () => {
+        li.remove();
+      });
+
+      list.prepend(li);
+    });
   }
 }
 
