dc4ff434c4554f542cdd532f996f099f80a73ea2
Kazue Hara <Kazue.Hara@jp.ricoh.com>
2024-09-16 15:35:18 +0900

    fix

--- ch15.11-15/ex03/server.js
+++ ch15.11-15/ex03/server.js
@@ -244,8 +244,9 @@ function cookieAuthzMiddleware(_url, req, res, params) {
 function corsMiddleware(_url, _req, res) {
   // TODO: CORS に必要なヘッダを複数設定する
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   return true;
 }
 
