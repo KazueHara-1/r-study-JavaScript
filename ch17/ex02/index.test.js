c23d8014d035b535f6a54521704df72e19bb9fd7
Kazue Hara <Kazue.Hara@jp.ricoh.com>
2024-11-17 11:49:29 +0900

    途中まで

--- ch17/ex02/index.test.js
+++ ch17/ex02/index.test.js
@@ -1,5 +1,7 @@
-import { createIssue } from "./index.js";
+// https://github.com/github/branch-deploy/issues/306
+import "@octokit/core";
 jest.mock("@octokit/core");
+import { createIssue } from "./index.js";
 
 test("Test createIssue", async () => {
   const TITLE = "Issue title";
